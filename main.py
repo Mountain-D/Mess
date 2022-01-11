6fab4de1c0edbc79917cab9cecb6188a6893dc8d
Mountain-D <dkhvesko@gmail.com>
2022-01-11 04:07:43 +0200

    betta

--- main.py
+++ main.py
@@ -15,12 +15,24 @@ display = pygame.Surface((900, 900))
 
 player_img = pygame.image.load('junk/player_img.png')
 
-tileset_img = pygame.image.load('junk/tilesetmelkas.png').convert()
+tileset_img = pygame.image.load('junk/tilesetmelkas.png')
 
 tile_1 = pygame.Surface.subsurface(tileset_img, (19, 147, 64, 36))
 tile_2 = pygame.Surface.subsurface(tileset_img, (147, 73, 64, 36))
 tile_3 = pygame.Surface.subsurface(tileset_img, (147, 13, 64, 60))
 tile_4 = pygame.Surface.subsurface(tileset_img, (147, 147, 64, 36))
+
+tile_1_rect = pygame.Rect(tile_1.get_rect())
+tile_2_rect = pygame.Rect(tile_2.get_rect())
+tile_3_rect = pygame.Rect(tile_3.get_rect())
+tile_4_rect = pygame.Rect(tile_4.get_rect())
+
+def collision_test(rect, tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
 
 from map import map_data
 
@@ -57,19 +69,20 @@ while True:
     if moving_down == True:
         player_location[1] += 4
 
+
     for y, row in enumerate(map_data):
         for x, tile in enumerate(row):
             if tile == 1:
-                pygame.draw.rect(tile_1, (255,255,255), pygame.Rect(tile_1.get_rect()))
+                pygame.draw.rect(tile_1,(255, 255, 255), tile_1_rect)
                 display.blit(tile_1, ((160 + x * 32 - y * 32) - scroll[0], (100 + x * 16 + y * 16) - scroll[1]))
             if tile == 2:
-                pygame.draw.rect(tile_2, (255, 255, 255), pygame.Rect(tile_1.get_rect()))
+                pygame.draw.rect(tile_2, (255, 255, 255), tile_2_rect)
                 display.blit(tile_2, ((160 + x * 32 - y * 32) - scroll[0], (100 + x * 16 + y * 16) - scroll[1]))
             if tile == 3:
-                pygame.draw.rect(tile_3, (255, 255, 255), pygame.Rect(tile_1.get_rect()))
+                pygame.draw.rect(tile_3, (255, 255, 255), tile_3_rect)
                 display.blit(tile_3, ((160 + x * 32 - y * 32) - scroll[0], (76 + x * 16 + y * 16) - scroll[1]))
             if tile == 4:
-                pygame.draw.rect(tile_4, (255, 255, 255), pygame.Rect(tile_1.get_rect()))
+                pygame.draw.rect(tile_4, (255, 255, 255), tile_4_rect)
                 display.blit(tile_4, ((160 + x * 32 - y * 32) - scroll[0], (100 + x * 16 + y * 16) - scroll[1]))
 
 
