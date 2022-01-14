87b5b5790c5b5389f3bb8d392e2526159c7640fe
Mountain-D <dkhvesko@gmail.com>
2022-01-14 18:54:31 +0200

    betta

--- main.py
+++ main.py
@@ -12,16 +12,7 @@ WINDOW_SIZE = (1024, 800)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 display = pygame.Surface((900, 900))
 
-#MEDIA JUNK UPLOAD
-
-player_img = pygame.image.load('junk/player_img.png')
-player_swim = pygame.image.load('junk/player_img_swim.png')
-
-tileset_img = pygame.image.load('junk/tilesetmelkas.png')
-
-tile_1 = pygame.Surface.subsurface(tileset_img, (19, 147, 64, 36))
-tile_2 = pygame.Surface.subsurface(tileset_img, (147, 73, 64, 36))
-tile_3 = pygame.Surface.subsurface(tileset_img, (147, 147, 64, 36))
+                ###
 
 #SPRITE STUFF
 
@@ -31,38 +22,46 @@ class Tile(pygame.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect()
 
-tile_water = Tile(tile_1)
-tile_grass = Tile(tile_2)
-tile_dirt = Tile(tile_3)
+tileset_img = pygame.image.load('junk/tilesetmelkas.png')
+tile_water = Tile(pygame.Surface.subsurface(tileset_img, (19, 147, 64, 36)))
+tile_grass = Tile(pygame.Surface.subsurface(tileset_img, (147, 73, 64, 36)))
+tile_dirt = Tile(pygame.Surface.subsurface(tileset_img, (147, 147, 64, 36)))
 
-water_group = pygame.sprite.Group()
-grass_group = pygame.sprite.Group()
-dirt_group = pygame.sprite.Group()
+tile_group = pygame.sprite.Group()
 
-water_group.add(tile_water)
-grass_group.add(tile_grass)
-dirt_group.add(tile_dirt)
+tile_group.add(tile_water)
+tile_group.add(tile_grass)
+tile_group.add(tile_dirt)
 
 #player sprite not done
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, image):
         super().__init__()
-        self.sprites = []
-        self.is_swim = False
-        self.sprites.append(pygame.image.load('junk/player_img.png'))
-        self.sprites.append(pygame.image.load('junk/player_img_swim.png'))
-        self.walk_sprite = 0
-        self.image = self.sprites[self.walk_sprite]
+        self.image = image
         self.rect = self.image.get_rect()
+    def player_location(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
+    def walking(self):
+        self.is_walk = True
 
     def swim(self):
         self.is_swim = True
 
-    def update(self):
-        if self.is_swim == True:
-            self.walk_sprite = 1
+   def update(self):
+       if self.is_swim == True:
 
-            self.image = self.sprites[self.walk_sprite]
+
+
+
+walking = Player(pygame.image.load('junk/player_img.png'))
+swiming = Player(pygame.image.load('junk/player_img_swim.png'))
+
+player_group = pygame.sprite.Group()
+
+player_group.add(walking)
+player_group.add(swiming)
 
 #MAP READ
 
@@ -70,7 +69,7 @@ f = open('junk/map.txt')
 map_data = [[int(column) for column in row] for row in f.read().split('\n')]
 f.close()
 
-#??? NEED TO ORGANIZE THIS
+#???scroll and move NEED TO ORGANIZE THIS
 
 moving_right = False
 moving_left = False
@@ -81,21 +80,23 @@ true_scroll = [0, 0]
 
 player_location = [400, 400]
 
+'''
 player_rect = pygame.Rect(player_location[0], player_location[1], player_img.get_width(), player_img.get_height())
+'''
 
 #GAME LOOP mess as well
 
 while True:
     display.fill((0, 0, 0))
 
-    true_scroll[0] += (player_rect.x-true_scroll[0]-420)/10
-    true_scroll[1] += (player_rect.y-true_scroll[1]-420)/10
+    true_scroll[0] += (Player.x - true_scroll[0] - 420) / 10
+    true_scroll[1] += (Player.y - true_scroll[1] - 420) / 10
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
 
-    player_rect.x = player_location[0]
-    player_rect.y = player_location[1]
+    Player.x = player_location[0]
+    Player.y = player_location[1]
 
     player_movement = [0, 0]
 
